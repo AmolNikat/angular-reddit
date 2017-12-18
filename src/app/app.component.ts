1b6c0b7fd04d89f7637d4b05e1e80885fea4a6de
amol <amolnikat13@gmail.com>
2017-12-18 23:36:25 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from "./article/article.model";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  //title = 'app';
+
+  articles:Article[];
+
+  constructor(){
+    this.articles = [
+      new Article('Angular 1','http://angular.io',10),
+      new Article('Angular 2','http://angular.io',20),
+      new Article('Angular 4','http://angular.io',30),
+      new Article('Angular 5','http://angular.io',40),
+
+    ];
+  }
+  addArticle(title:HTMLInputElement,link:HTMLInputElement):boolean{
+
+    this.articles.push(new Article(title.value,link.value,0));
+    title.value='';
+    link.value='';
+    console.log(`Adding Article with title: ${title.value} and link :${link.value}`);
+    return false;
+
+  }
+
+  sortedArticles():Article[]{
+    return this.articles.sort((a:Article,b:Article)=>b.votes-a.votes);
+  }
 }
